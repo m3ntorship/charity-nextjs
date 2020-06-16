721ef39a0b934000c9abfab9663e40d272a82bed
elhaw <abd.elsayed@hotmail.com>
2020-06-16 16:15:03 +0400

    used charity-cms url

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const Home = ({ data }) => {
 };
 
 export async function getServerSideProps() {
-  const res = await fetch("http://localhost:1337/articles");
+  const res = await fetch("https://charity-cms-dev.m3ntorship.net/articles");
   const jsonData = await res.json();
 
   return {
