fa67dc082f75448a4026c6961633d063c4d0da44
elhaw <abd.elsayed@hotmail.com>
2020-06-17 02:08:31 +0400

    used useCharityApi function

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Layout from "../components/Layout";
-import fetch from "node-fetch";
-
-const Home = ({ data }) => {
-  const homeArticles = data.filter(({ is_in_home }) => is_in_home === true);
+import { charityAPI } from "../clients";
+const Home = ({ articlesData }) => {
+  const homeArticles = articlesData.filter(
+    ({ is_in_home }) => is_in_home === true
+  );
   return (
     <>
       <Layout>
         <h1 className="text-pink-700">This is the Home page</h1>
         <div className="flex justify-center text-center">
           {homeArticles.map(
-            ({ title, description, thumbnail: { url: imgUrl },id }) => {
+            ({ title, description, thumbnail: { url: imgUrl }, id }) => {
               return (
-                <article key = {id}>
+                <article key={id}>
                   <img src={imgUrl} alt="" />
                   <h1>{title}</h1>
                   <p>{description}</p>
@@ -26,14 +27,13 @@ const Home = ({ data }) => {
 };
 
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.url}/articles`);
-  const jsonData = await res.json();
-
-  return {
-    props: {
-      data: jsonData,
-    },
-  };
+  return Promise.all([charityAPI("/articles")]).then(
+    ([{ data: articlesData }]) => {
+      return {
+        props: { articlesData },
+      };
+    }
+  );
 }
 
 export default Home;
