ccf5892089e12a142b2f2d38191f6a756754b6a0
elhaw <abd.elsayed@hotmail.com>
2020-06-17 21:18:55 +0400

    imported fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import "../styles/index.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 
-function MyApp({ Component, pageProps}) {
-
-
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-
 
 export default MyApp;
