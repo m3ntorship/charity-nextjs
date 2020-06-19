808ecc81b56c3a2b8f3a3fcefba5c549a256c1e8
elhaw <abd.elsayed@hotmail.com>
2020-06-19 17:43:21 +0400

    added props to navigationLink component

--- components/shared/NavigationLink/index.js
+++ components/shared/NavigationLink/index.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
 import cn from "classnames";
 
-const NavigationLink = () => {
+const NavigationLink = ({ url, text, linkClassName, secondaryClassName }) => {
   return (
-    <li >
-            some link
+    <li className={cn("text-center", secondaryClassName)}>
+      <Link href={url}>
+        <a className={cn("", linkClassName)}>{text}</a>
+      </Link>
     </li>
   );
 };
 
-
-export default NavigationLink
+export default NavigationLink;
