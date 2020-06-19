4214ff445be1276e64cea37c0a4167de4ba2301d
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2020-06-19 22:07:32 +0400

    Feature/charity nextjs style (#6)
    * added style files for every component
    
    * added article search style
    
    * added image for wrok style
    
    * added global style file
    
    * added Head component in layout page to add link to Merriweather
    
    * added font-awesome

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,6 +1,18 @@
-
+import Head from 'next/head'
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      {children}
+    </>
+  );
 };
 
 export default Layout;
