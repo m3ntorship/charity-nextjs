35db787d80adfd1b2c4b1c694ecbeee58a1a70e1
AbdoMagdy <abdoman46@hotmail.com>
2020-06-20 15:41:52 +0200

    use Layout with articles page

--- pages/articles.js
+++ pages/articles.js
@@ -1,31 +1,66 @@
 import { ArticlesList } from "../components/NewsAndArticles";
 import { Banner } from "../components/ArticleBanner";
 import { VolunteeringBanner } from "../components/VolunteeringBanner";
+import Layout from "../components/Layout";
 import { charityAPI } from "../clients";
-const articles = ({ articlesListData, articlesPageData }) => {
+const articles = ({
+  articlesListData,
+  articlesPageData,
+  footerData,
+  ContactsData,
+  logoData,
+  socialMediasData,
+  pagesData,
+}) => {
   return (
     <>
-      <Banner data={articlesPageData} />
-      <ArticlesList articles={articlesListData} />
-      <VolunteeringBanner data={articlesPageData} />
+      <Layout
+        footerData={footerData}
+        ContactsData={ContactsData}
+        logoData={logoData}
+        socialMediasData={socialMediasData}
+        pagesData={pagesData}
+      >
+        <Banner data={articlesPageData} />
+        <ArticlesList articles={articlesListData} />
+        <VolunteeringBanner data={articlesPageData} />
+      </Layout>
     </>
   );
 };
 
 export function getServerSideProps() {
   return Promise.all([
-    charityAPI("/pages?published=true"),
+    charityAPI("/main-contacts"),
+    charityAPI("/logo"),
+    charityAPI("/socialmedias"),
+    charityAPI("/pages"),
+    charityAPI("/footer"),
     charityAPI("/news-and-articles"),
-  ]).then(([{ data: pagesData }, { data: articlesListData }]) => {
-    const [articlesPageData] = pagesData.filter(
-      (pageData) => pageData.name === "articles"
-    );
-    return {
-      props: {
-        articlesListData: articlesListData.home_articles,
-        articlesPageData,
-      },
-    };
-  });
+  ]).then(
+    ([
+      { data: ContactsData },
+      { data: logoData },
+      { data: socialMediasData },
+      { data: pagesData },
+      { data: footerData },
+      { data: articlesListData },
+    ]) => {
+      const [articlesPageData] = pagesData.filter(
+        (pageData) => pageData.name === "articles"
+      );
+      return {
+        props: {
+          ContactsData,
+          logoData,
+          socialMediasData,
+          pagesData,
+          footerData,
+          articlesListData: articlesListData.home_articles,
+          articlesPageData,
+        },
+      };
+    }
+  );
 }
 export default articles;
