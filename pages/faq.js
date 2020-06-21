e7b7f5674d1ba519c720d0f7ed8541ba580da5b1
AbdoMagdy <abdoman46@hotmail.com>
2020-06-21 18:18:23 +0200

    add dummy pages

---
+++ pages/faq.js
@@ -0,0 +1,52 @@
+import Layout from "../../components/Layout";
+import { charityAPI } from "../../clients";
+
+const Faq = ({
+  footerData,
+  ContactsData,
+  logoData,
+  socialMediasData,
+  pagesData,
+}) => {
+  return (
+    <Layout
+      footerData={footerData}
+      ContactsData={ContactsData}
+      logoData={logoData}
+      socialMediasData={socialMediasData}
+      pagesData={pagesData}
+    >
+      Faq components goes here
+    </Layout>
+  );
+};
+
+export function getServerSideProps() {
+  return Promise.all([
+    charityAPI("/main-contacts"),
+    charityAPI("/logo"),
+    charityAPI("/socialmedias"),
+    charityAPI("/pages"),
+    charityAPI("/footer"),
+  ]).then(
+    ([
+      { data: ContactsData },
+      { data: logoData },
+      { data: socialMediasData },
+      { data: pagesData },
+      { data: footerData },
+    ]) => {
+      return {
+        props: {
+          ContactsData,
+          logoData,
+          socialMediasData,
+          footerData,
+          pagesData,
+        },
+      };
+    }
+  );
+}
+
+export default Faq;
