e190efb44cc32ddcd01f198ebf5b189e53f4d3a8
Tramsi <tramsi2014@gmail.com>
2020-06-21 18:41:34 +0200

    Fixed header navigation bug

--- components/HeaderNavigation/index.js
+++ components/HeaderNavigation/index.js
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
+import cn from "classnames";
 import Logo from "../Logo";
+import NavigationLink from "../NavigationLink";
 import Link from "next/link";
 
-import NavigationLink from "../shared/NavigationLink";
+const HeaderNavigation = ({ logoData, pagesData, contactsData }) => {
+  const [isOpen, setOpen] = useState(false);
 
-const HeaderNavigation = ({ logoData, ContactsData, pagesData }) => {
   return (
     <section className="header-nav py-5 px-0">
       <div className="container sm:flex sm:justify-between">
@@ -20,28 +23,30 @@ const HeaderNavigation = ({ logoData, ContactsData, pagesData }) => {
               <button
                 type="button"
                 className="text-gray-600 block"
-                // onClick={() => {
-                //   setOpen(!isOpen);
-                // }}
+                onClick={() => {
+                  setOpen(!isOpen);
+                }}
               >
-                {/* {isOpen ? (
-                  <i className="fas fa-times text-c100"></i>
+                {isOpen ? (
+                  <i className="fas fa-facebook text-c100"></i>
                 ) : (
                   <i className="fas fa-bars text-c100"></i>
-                )} */}
+                )}
               </button>
             </div>
           </div>
           <ul
-            className="block nav-links"
+            className={cn("block nav-links", {
+              hidden: !isOpen,
+            })}
           >
             {pagesData
               .filter((page) => page.show_in_navigation)
-              .map(({ id, link: { url, text } }) => (
+              .map((page) => (
                 <NavigationLink
-                  key={id}
-                  url={url}
-                  text={text}
+                  key={page.id}
+                  url={page.link.url}
+                  title={page.link.text}
                   secondaryClassName="sm:mx-4 sm:font-bold nav-link"
                   linkClassName="text-c600 hover:text-c100 p-2 mx-1 block"
                 />
@@ -49,43 +54,46 @@ const HeaderNavigation = ({ logoData, ContactsData, pagesData }) => {
           </ul>
         </div>
         <div className="hidden lg:block contacts text-sm mx-6">
-          {ContactsData.filter(
-            (contact) =>
-              contact.sub_title === "Email address" ||
-              contact.sub_title === "Phone line"
-          ).map(
-            ({
-              _id,
-              title,
-              url,
-              sub_title,
-              icon: { url: iconUrl, name: IconName },
-            }) => {
-              return (
-                <div key={_id} className="contact px-5 lg:my-5">
-                  <div className="contact-icon">
-                    <img className="h-auto" src={iconUrl} alt={IconName} />
+          {contactsData
+            .filter(
+              (contact) =>
+                contact.sub_title === "Email address" ||
+                contact.sub_title === "Phone line"
+            )
+            .map(
+              ({
+                _id,
+                title,
+                url,
+                sub_title,
+                icon: { url: iconUrl, name: IconName },
+              }) => {
+                return (
+                  <div key={_id} className="contact px-5 lg:my-5">
+                    <div className="contact-icon">
+                      <img className="h-auto" src={iconUrl} alt={IconName} />
+                    </div>
+                    <div className="information">
+                      <a
+                        className="block text-c100 font-bold hover:text-c100"
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {title}
+                      </a>
+                      <small className="information-small font-bold leading-normal text-c600">
+                        {sub_title}
+                      </small>
+                    </div>
                   </div>
-                  <div className="information">
-                    <a
-                      className="block text-c100 font-bold hover:text-c100"
-                      href={url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {title}
-                    </a>
-                    <small className="information-small font-bold leading-normal text-c600">
-                      {sub_title}
-                    </small>
-                  </div>
-                </div>
-              );
-            }
-          )}
+                );
+              }
+            )}
         </div>
       </div>
     </section>
   );
 };
+
 export default HeaderNavigation;
