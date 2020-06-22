d3d1254779d66ba6812dc355100f6a2e585b1db2
elhaw <abd.elsayed@hotmail.com>
2020-06-22 12:05:43 +0400

    added language parameter in url in navigationLink component

--- components/NavigationLink/index.jsx
+++ components/NavigationLink/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import cn from "classnames";
 import Link from "next/link";
+import useI18n from "../../hooks/use-i18n";
 
 const NavigationLink = ({ url, title, secondaryClassName, linkClassName }) => {
+  const i18n = useI18n()
+  const currentLocale = i18n.activeLocale
   return (
     <li className={cn("text-center", secondaryClassName)}>
-      <Link href={url}>
+      <Link href={`/${currentLocale}${url}`}>
         <a className={cn("", linkClassName)}>{title}</a>
       </Link>
     </li>
