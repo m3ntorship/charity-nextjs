0bf4abea668a4934d2fff62081ca480890e5fbf8
elhaw <abd.elsayed@hotmail.com>
2020-06-22 12:01:52 +0400

    added default destination

--- next.config.js
+++ next.config.js
@@ -2,4 +2,15 @@ module.exports = {
   env: {
     url: process.env.REACT_APP_STRAPI_URL,
   },
+  experimental: {
+    redirects() {
+      return [
+        {
+          source: "/",
+          permanent: true,
+          destination: "/en",
+        },
+      ];
+    },
+  },
 };
