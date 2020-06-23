240e9c85dd68c4c1848f50099129f2aee1162b82
elhaw <abd.elsayed@hotmail.com>
2020-06-23 18:18:50 +0400

    convert class component to function component

--- components/Footer/Links/index.jsx
+++ components/Footer/Links/index.jsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-export default class Links extends React.Component {
-  render() {
-    const links = this.props.links.map(link => {
-      return (
-        <li key={link.id} className="pb-4">
-          <Link href={link.url}>
-          <a>
-            {link.text}
-            </a>
-            </Link>
-        </li>
-      );
-    });
+const Links = ({ links, title }) => {
+  const allLinks = links.map(({ id, text, url }) => {
     return (
-      <div className="footer-card lg:my-0 my-6">
-        <h3 className="text-c000 text-lg font-semibold mb-8">
-          {this.props.title}
-        </h3>
-        <ul className="flex flex-col flex-wrap h-48">{links}</ul>
-      </div>
+      <li key={id} className="pb-4">
+        <Link href={url}>
+          <a>{text}</a>
+        </Link>
+      </li>
     );
-  }
-}
+  });
+  return (
+    <div className="footer-card lg:my-0 my-6">
+      <h3 className="text-c000 text-lg font-semibold mb-8">"{title}"</h3>
+      <ul className="flex flex-col flex-wrap h-48">{allLinks}</ul>
+    </div>
+  );
+};
+
+export default Links;
