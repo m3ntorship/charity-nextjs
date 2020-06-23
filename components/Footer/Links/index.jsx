2b0d84e1ab05ec2ec7c3d3e5bedd384b5aba84e6
elhaw <abd.elsayed@hotmail.com>
2020-06-23 18:21:27 +0400

    moved all links in the return

--- components/Footer/Links/index.jsx
+++ components/Footer/Links/index.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import Link from "next/link";
 
 const Links = ({ links, title }) => {
-  const allLinks = links.map(({ id, text, url }) => {
-    return (
-      <li key={id} className="pb-4">
-        <Link href={url}>
-          <a>{text}</a>
-        </Link>
-      </li>
-    );
-  });
   return (
     <div className="footer-card lg:my-0 my-6">
-      <h3 className="text-c000 text-lg font-semibold mb-8">"{title}"</h3>
-      <ul className="flex flex-col flex-wrap h-48">{allLinks}</ul>
+      <h3 className="text-c000 text-lg font-semibold mb-8">{title}</h3>
+      <ul className="flex flex-col flex-wrap h-48">
+        {links.map(({ id, text, url }) => {
+          return (
+            <li key={id} className="pb-4">
+              <Link href={url}>
+                <a>{text}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
