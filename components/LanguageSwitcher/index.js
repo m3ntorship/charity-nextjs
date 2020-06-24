739b0e4359b92d78c77a98a38b5d7f59d9a1cf63
elhaw <abd.elsayed@hotmail.com>
2020-06-24 15:19:40 +0400

    add country flags for language selector

--- components/LanguageSwitcher/index.js
+++ components/LanguageSwitcher/index.js
@@ -1,24 +1,40 @@
 import React from "react";
 import Link from "next/link";
-import { useRouter } from 'next/router'
+import useI18n from "../../hooks/use-i18n";
+import { useRouter } from "next/router";
 const LanguageSwitcher = () => {
   const router = useRouter();
-  const {asPath} = router
-  let pageName = asPath.substring(3)
+  const i18n = useI18n();
+  const currentLocale = i18n.activeLocale;
+  const { asPath } = router;
+  let pageName = asPath.substring(3);
   return (
     <>
-      <li className ="inline-block language_elector" >
-        <Link href={`/ar${pageName}`}>
-          <a>
-            اللغة العربية
-          </a>
-        </Link>
-      </li>
-      <li  className ="inline-block language_elector">
-        <Link href={`/en${pageName}`}>
-          English
-        </Link>
-      </li>
+        {currentLocale === "ar" ? (
+          <li className="inline-block language_elector">
+            <Link href={`/en${pageName}`}>
+              <a>
+                <img
+                  className="w-8 h-6"
+                  src="../../static/flag-icons/uk-en.png"
+                  alt=""
+                  title = {`change to English `}
+                />
+              </a>
+            </Link>
+          </li>
+        ) : (
+          <Link href={`/ar${pageName}`}>
+            <a>
+              <img
+                className="w-8 h-6"
+                src="../../static/flag-icons/eg-ar.png"
+                alt=""
+                title = "التحويل للغة العربية"
+              />
+            </a>
+          </Link>
+        )}
     </>
   );
 };
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,6 +1,5 @@
 // import Header from "../Header";
 import { Footer } from "../Footer";
-import { useEffect, useState } from "react";
 import Head from "next/head";
 import HeaderTop from "../HeaderTop";
 import HeaderNavigation from "../HeaderNavigation";
@@ -16,8 +15,6 @@ const Layout = ({
 }) => {
   const i18n = useI18n();
   const currentLocale = i18n.activeLocale;
-  const [font, setFont] = useState("");
-
   return (
     <>
       <Head>
@@ -26,58 +23,23 @@ const Layout = ({
         {currentLocale === "ar" && (
           <link rel="stylesheet" href={`../../static/styles/style-ar.css`} />
         )}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Freckle+Face&family=Roboto+Slab:wght@100;200;300;700&family=Tangerine:wght@400;700&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;700&family=El+Messiri:wght@400;600;700&family=Jomhuria&family=Mirza:wght@400;600&family=Tajawal:wght@200;500;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-      <div
-        style={{
-          fontFamily: font,
-        }}
-      >
-        <div>
-          <select
-            onChange={(evt) => {
-              setFont(evt.target.value);
-            }}
-          >
-            <option value="" disabled hidden selected>
-              Select the font
-            </option>
-            <option value="'Freckle Face', cursive">
-              Freckle Face', cursive
-            </option>
-            <option value="'Roboto Slab', serif">'Roboto Slab', serif</option>
-            <option value="'Tangerine', cursive">'Tangerine', cursive</option>
-            <option value="'Almarai', sans-serif">
-              'Almarai', sans-serif;
-            </option>
-            <option value="'El Messiri', sans-serif">
-              'El Messiri', sans-serif;
-            </option>
-            <option value="'Jomhuria', cursive">'Jomhuria', cursive;</option>
-            <option value="'Mirza', cursive">'Mirza', cursive;</option>
-            <option value="'Tajawal', sans-serif">
-              'Tajawal', sans-serif;
-            </option>
-          </select>
-        </div>
-        <header>
-          <HeaderTop socialMediasData={socialMediasData} />
-          <HeaderNavigation
-            logoData={logoData}
-            contactsData={contactsData}
-            pagesData={pagesData}
+        {currentLocale === "ar" && (
+          <link
+            href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700&display=swap"
+            rel="stylesheet"
           />
-        </header>
-        <main>{children}</main>
-        <Footer data={footerData} />
-      </div>
+        )}
+      </Head>
+      <header>
+        <HeaderTop socialMediasData={socialMediasData} />
+        <HeaderNavigation
+          logoData={logoData}
+          contactsData={contactsData}
+          pagesData={pagesData}
+        />
+      </header>
+      <main>{children}</main>
+      <Footer data={footerData} />
     </>
   );
 };
