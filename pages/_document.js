e9a3965d44af58370ae5cdb8102e497236ebcc42
Tramsi <tramsi2014@gmail.com>
2020-06-27 00:31:57 +0200

    change html dir and font based on url

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,11 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
+    let lng = this.props.__NEXT_DATA__.query.lng;
     return (
-      <Html dir="ltr">
+      <Html dir={lng === "ar" ? "rtl" : "ltr"}>
         <Head />
-        <body>
+        <body className={lng === "ar" ? "font-ar" : "font-en"}>
           <Main />
           <NextScript />
         </body>
