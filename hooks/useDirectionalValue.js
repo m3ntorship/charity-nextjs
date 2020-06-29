26d2a6927b217e402249326404217c1a854e9fe3
Tramsi <tramsi2014@gmail.com>
2020-06-29 15:55:51 +0200

    Added direction case to useDirectionalValue

--- hooks/useDirectionalValue.js
+++ hooks/useDirectionalValue.js
@@ -5,6 +5,16 @@ export const useDirectionalValue = (value, type = 'number') => {
     case 'number': {
       return activeDirection === 'rtl' ? -1 * value : value;
     }
+    case 'direction': {
+      if (value === 'left') {
+        return activeDirection === 'rtl' ? 'right' : 'left';
+      }
+      if (value === 'right') {
+        return activeDirection === 'rtl' ? 'left' : 'right';
+      } else {
+        return value;
+      }
+    }
     default:
       return value;
   }
