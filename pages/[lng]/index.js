305fc2962dc83af9b41db3345473e67e8f1a2db8
Tramsi <tramsi2014@gmail.com>
2020-06-29 21:30:21 +0200

    Fixed Carousel styles in causes section issue

--- pages/[lng]/index.js
+++ pages/[lng]/index.js
@@ -1,20 +1,20 @@
-import { HeaderCarousel } from "../../components/HeaderCarousel";
-import { Welcome } from "../../components/Welcome";
-import { Activities } from "../../components/Activities";
-import { FeaturedBanner } from "../../components/FeaturedBanner";
-import { Causes } from "../../components/Causes";
-import { Numbers } from "../../components/Numbers";
-import { UpcomingEventsSection } from "../../components/UpcomingEvents";
-import { Testimonials } from "../../components/Testimonials";
-import { WorkStyle } from "../../components/WorkStyle";
-import { News } from "../../components/NewsAndArticles";
-import { Sponsers } from "../../components/Sponsers";
-import { ContactInfo } from "../../components/ContactInfo";
-import { charityAPI } from "../../clients";
-import Layout from "../../components/Layout";
-import Head from "next/head";
-import useI18n from "../../hooks/use-i18n";
-import { contentLanguageMap } from "../../lib/i18n";
+import { HeaderCarousel } from '../../components/HeaderCarousel';
+import { Welcome } from '../../components/Welcome';
+import { Activities } from '../../components/Activities';
+import { FeaturedBanner } from '../../components/FeaturedBanner';
+import { Causes } from '../../components/Causes';
+import { Numbers } from '../../components/Numbers';
+import { UpcomingEventsSection } from '../../components/UpcomingEvents';
+import { Testimonials } from '../../components/Testimonials';
+import { WorkStyle } from '../../components/WorkStyle';
+import { News } from '../../components/NewsAndArticles';
+import { Sponsers } from '../../components/Sponsers';
+import { ContactInfo } from '../../components/ContactInfo';
+import { charityAPI } from '../../clients';
+import Layout from '../../components/Layout';
+import Head from 'next/head';
+import useI18n from '../../hooks/use-i18n';
+import { contentLanguageMap } from '../../lib/i18n';
 const Home = ({
   headerCarouselData,
   welcomeData,
@@ -31,19 +31,19 @@ const Home = ({
   contactsData,
   logoData,
   socialMediasData,
-  pagesData,
+  pagesData
 }) => {
-  let featuredCauseData = (data) => {
+  let featuredCauseData = data => {
     if (data) {
-      let featuredCause = data.causes.find((cause) => {
+      let featuredCause = data.causes.find(cause => {
         return cause.is_featured;
       });
       return {
-        featuredCause,
+        featuredCause
       };
     } else {
       return {
-        featuredCause: null,
+        featuredCause: null
       };
     }
   };
@@ -67,7 +67,7 @@ const Home = ({
       <Welcome data={welcomeData} />
       <Activities data={activitiesData} />
       <FeaturedBanner data={featuredBannerData} />
-      {/* <Causes data={causesData} /> */}
+      <Causes data={causesData} />
       <Numbers data={numbersData} />
       <UpcomingEventsSection
         data={upcomingEventsData}
@@ -85,22 +85,22 @@ export async function getServerSideProps({ params: { lng } }) {
   const { default: lngDict = {} } = await import(`../../locales/${lng}.json`);
   const getCharityAPI = charityAPI(lng);
   return Promise.all([
-    getCharityAPI("/main-contacts"),
-    getCharityAPI("/logo"),
-    getCharityAPI("/socialmedias"),
-    getCharityAPI("/pages"),
-    getCharityAPI("/main-carousels"),
-    getCharityAPI("/welcome-section"),
-    getCharityAPI("/what-we-do"),
-    getCharityAPI("/featured-banner"),
-    getCharityAPI("/popular-causes"),
-    getCharityAPI("/speaking-numbers"),
-    getCharityAPI("/upcoming-events"),
-    getCharityAPI("/what-they-say"),
-    getCharityAPI("/how-we-work"),
-    getCharityAPI("/news-and-articles"),
-    getCharityAPI("/Sponsers"),
-    getCharityAPI("/footer"),
+    getCharityAPI('/main-contacts'),
+    getCharityAPI('/logo'),
+    getCharityAPI('/socialmedias'),
+    getCharityAPI('/pages'),
+    getCharityAPI('/main-carousels'),
+    getCharityAPI('/welcome-section'),
+    getCharityAPI('/what-we-do'),
+    getCharityAPI('/featured-banner'),
+    getCharityAPI('/popular-causes'),
+    getCharityAPI('/speaking-numbers'),
+    getCharityAPI('/upcoming-events'),
+    getCharityAPI('/what-they-say'),
+    getCharityAPI('/how-we-work'),
+    getCharityAPI('/news-and-articles'),
+    getCharityAPI('/Sponsers'),
+    getCharityAPI('/footer')
   ]).then(
     ([
       { data: contactsData },
@@ -118,7 +118,7 @@ export async function getServerSideProps({ params: { lng } }) {
       { data: workStyleData },
       { data: newsData },
       { data: sponsersData },
-      { data: footerData },
+      { data: footerData }
     ]) => {
       return {
         props: {
@@ -139,8 +139,8 @@ export async function getServerSideProps({ params: { lng } }) {
           sponsersData,
           footerData,
           lngDict,
-          lng,
-        },
+          lng
+        }
       };
     }
   );
