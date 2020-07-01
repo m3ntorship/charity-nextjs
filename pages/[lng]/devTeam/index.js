37180af3f72c9b2169df960d1ad2e76cd90b8886
Tramsi <tramsi2014@gmail.com>
2020-07-01 15:19:03 +0200

    Added devTeam page

---
+++ pages/[lng]/devTeam/index.js
@@ -0,0 +1,74 @@
+import Layout from '../../../components/Layout';
+import { charityAPI } from '../../../clients';
+import { PersonCardsSection } from '../../../components/PersonCardsSection';
+import { SecondaryBanner } from '../../../components/SecondaryBanner';
+
+const Volunteers = ({
+  footerData,
+  contactsData,
+  logoData,
+  socialMediasData,
+  pagesData,
+  devTeamPageData,
+  devTeamMembersData,
+  lng
+}) => {
+  return (
+    <Layout
+      footerData={footerData}
+      contactsData={contactsData}
+      logoData={logoData}
+      socialMediasData={socialMediasData}
+      pagesData={pagesData}
+    >
+      <div className="container">
+        <PersonCardsSection data={devTeamMembersData} lng={lng} />
+      </div>
+      <SecondaryBanner data={devTeamPageData} />
+    </Layout>
+  );
+};
+
+export async function getServerSideProps({ params: { lng } }) {
+  const { default: lngDict = {} } = await import(
+    `../../../locales/${lng}.json`
+  );
+  const getCharityAPI = charityAPI(lng);
+
+  return Promise.all([
+    getCharityAPI('/main-contacts'),
+    getCharityAPI('/logo'),
+    getCharityAPI('/socialmedias'),
+    getCharityAPI('/pages'),
+    getCharityAPI('/dev-team-members'),
+    getCharityAPI('/footer')
+  ]).then(
+    ([
+      { data: contactsData },
+      { data: logoData },
+      { data: socialMediasData },
+      { data: pagesData },
+      { data: devTeamMembersData },
+      { data: footerData }
+    ]) => {
+      const [devTeamPageData] = pagesData.filter(
+        pageData => pageData.name === 'Dev Team'
+      );
+      return {
+        props: {
+          contactsData,
+          logoData,
+          socialMediasData,
+          footerData,
+          pagesData,
+          devTeamMembersData,
+          devTeamPageData,
+          lng,
+          lngDict
+        }
+      };
+    }
+  );
+}
+
+export default Volunteers;
