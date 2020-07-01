33ffb2130955628bf7d3e0f0f17ec72b2f12ca62
user <great.nile@gmail.com>
2020-07-02 01:44:44 +0200

    dynamically add language knob

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,27 @@
+import React from 'react';
 import '../styles/index.css';
+import { boolean, select } from '@storybook/addon-knobs';
+import { addDecorator } from '@storybook/react';
+import I18n from '../lib/i18n';
+import langDictAr from '../locales/ar.json';
+import langDictEn from '../locales/en.json';
+
+addDecorator(storyFn => {
+  const lang = select(
+    'Language',
+    {
+      English: 'en',
+      Arabic: 'ar'
+    },
+    'en'
+  );
+  const langDict = lang == 'ar' ? langDictAr : langDictEn;
+  const dir = lang == 'ar' ? 'rtl' : 'ltr';
+  return (
+    <I18n lngDict={langDict} locale={lang}>
+      <div dir={dir} className={`font-${lang}`}>
+        {storyFn({ lang })}
+      </div>
+    </I18n>
+  );
+});
---
+++ stories/Banner/data.js
@@ -0,0 +1,30 @@
+export const en = {
+  text_primary:
+    'Making someone else’s dreams a reality is something that’s value is',
+  text_complementary: 'innumerable',
+  button_text: 'Start Donation',
+  button_url: '/nile',
+  image_top: {
+    url:
+      'https://s3.m3ntorship.net/charity-cms-dev/donation-banner-icon_37be254b77_7b2a8696a1.png'
+  },
+  image_background: {
+    url:
+      'https://s3.m3ntorship.net/charity-cms-dev/donation-banner-bg_15d2c4c3bf.jpeg'
+  }
+};
+
+export const ar = {
+  text_primary: 'تحويل أحلام شخص آخر إلى حقيقة أمر ذو قيمة',
+  text_complementary: 'شارك وبادر',
+  button_text: 'تبرع الآن',
+  button_url: '/donations',
+  image_top: {
+    url:
+      'https://s3.m3ntorship.net/charity-cms-dev/donation-banner-icon_37be254b77_7b2a8696a1.png'
+  },
+  image_background: {
+    url:
+      'https://s3.m3ntorship.net/charity-cms-dev/donation-banner-bg_15d2c4c3bf.jpeg'
+  }
+};
