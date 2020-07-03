58538ee0329c11b2972c6729bf071f93e1b84f09
Tramsi <tramsi2014@gmail.com>
2020-07-03 18:19:13 +0200

    Fixed animaiton in arabic rtl

--- components/PersonCard/index.jsx
+++ components/PersonCard/index.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react';
 import { useSpring, animated, useTransition } from 'react-spring';
 import { CircularLink } from '../shared/CircularLink';
+import { useDirectionalValue } from '../../hooks/useDirectionalValue';
+import useMedia from '../../Helpers/useMedia';
 
 export const PersonCard = ({ data }) => {
+  const isMobile = useMedia(['(min-width: 768px)'], [false], true);
   const [isHoverd, setIsHoverd] = useState(false);
   const backgroundFade = useSpring({
     opacity: isHoverd ? 0.3 : 0
   });
+  const iconAnmiationValue = useDirectionalValue(-30);
   const iconsTransition = useTransition(data.social_icons, data => data.id, {
-    from: { transform: 'translateX(-30%)', opacity: 0 },
     update: {
-      transform: isHoverd ? 'translateX(0)' : 'translateX(-30%)',
+      transform: isHoverd
+        ? 'translateX(0)'
+        : `translateX(${iconAnmiationValue}%)`,
       opacity: isHoverd ? 1 : 0
     },
     trail: 150
   });
-  console.log(iconsTransition);
   //Data destructuring
   const {
     member_name,
@@ -33,14 +37,12 @@ export const PersonCard = ({ data }) => {
           className="absolute w-full h-full bg-c100 left-0 top-0 pointer-events-none"
           style={backgroundFade}
         ></animated.div>
-        <div className="person-card__image-container__social-icons absolute  h-full flex flex-col justify-center items-start">
-          {iconsTransition.map(({ item: { url, fa_icon, id }, key, props }) => (
-            <animated.div
-              style={props}
-              className="my-1 flex justify-center items-center"
-              key={key}
-            >
-              <CircularLink url={url} fontClass={fa_icon} />
+        <div className="person-card__image-container__social-icons absolute flex justify-center items-start bottom-0">
+          {iconsTransition.map(({ item: { url, fa_icon, id }, props }) => (
+            <animated.div style={isMobile ? null : props} key={id}>
+              <div className="mx-2 md:mx-0 md:my-1 flex justify-center items-center">
+                <CircularLink url={url} fontClass={fa_icon} animation={true} />
+              </div>
             </animated.div>
           ))}
         </div>
@@ -50,7 +52,7 @@ export const PersonCard = ({ data }) => {
           src={imgUrl}
         />
       </div>
-      <div className="person-card__info my-8">
+      <div className="person-card__info my-10 md:my-8">
         <h4 className="person-card__info__name text-md text-c100 font-bold leading-tighter">
           {member_name}
         </h4>
