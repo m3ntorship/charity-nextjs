6e5931d8aa03bb16b442cd8a1195521b4a2a7226
Tramsi <tramsi2014@gmail.com>
2020-07-10 15:57:47 +0200

    Removed LinkNoPrefetch Component

--- components/shared/LinkNoPrefetch/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Link from 'next/link';
-
-const LinkNoPrefetch = ({ children, ...props }) => {
-  return <Link {...props}>{children}</Link>;
-};
-
-export default LinkNoPrefetch;
