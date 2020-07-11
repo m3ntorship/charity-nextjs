5a6092f3930812176e47fd304cb4c01b6ff83107
elhaw <abd.elsayed@hotmail.com>
2020-07-11 19:26:38 +0400

    handling erro if  data in soon component is empty

--- components/Soon/index.jsx
+++ components/Soon/index.jsx
@@ -1,21 +1,22 @@
-export const Soon = ({
-  data: {
+export const Soon = data => {
+  let {
     comming_soon_main_message,
     comming_soon_second_message,
     comming_soon_third_message,
     comming_soon_image: { url } = {}
-  }
-}) => {
+  } = data;
   return (
     <div className="flex flex-col text-c100 items-center w-full text-center py-16 border-t">
       <div>
         <img src={url} alt="soon" />
       </div>
       <p className="text-large leading-none my-10 mb-4 font-semibold">
-        {comming_soon_main_message}
+        {!data.statusCode && comming_soon_main_message}
       </p>
       <p>{comming_soon_second_message}</p>
-      <p className="text-lg">{comming_soon_third_message}</p>
+      <p className="text-lg">
+        {!data.statusCode && comming_soon_third_message}
+      </p>
     </div>
   );
 };
